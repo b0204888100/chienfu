9aaaeb4580c3d29c31cfdb5f6d4a233e3a4963e3
b0204888100 <b0204888100@gmail.com>
2017-04-23 23:55:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-import json
-import httplib2
-import logging
-from apiclient import discovery
-from google.appengine.api import memcache
-from oauth2client.contrib.appengine import AppAssertionCredentials
-
 from flask import Flask
-
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
-INSTANCE_NAME = 'instance-1'
-INSTANCE_ZONE = 'asia-east1-a'
-PROJECT = 'esoteric-cider-165206'
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
 
 
 @app.route('/')
-def hello_world():
+def hello():
+    """Return a friendly HTTP greeting."""
     return 'Hello World!'
-
 
 @app.route('/vm/start')
 def start_vm():
@@ -26,8 +18,8 @@ def start_vm():
     compute = discovery.build('compute', 'v1', http=http)
     result = compute.instances().start(instance=INSTANCE_NAME, zone=INSTANCE_ZONE, project=PROJECT).execute()
     logging.debug(result)
-    return json.dumps(result, indent=4)
 
-
-if __name__ == '__main__':
-    app.run()
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, nothing at this URL.', 404
